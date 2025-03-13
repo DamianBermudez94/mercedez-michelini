5042919c4c2c7842dc0e38d520265f66118a1065
DamianBermudez94 <bermudezdamian7@gamil.com>
2025-03-12 23:38:24 -0300

    se le agregaron animaciones a los bonotes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
       <div className="absolute inset-0 flex items-center flex-col justify-center text-center p-5">
         <h1 className="text-4xl font-bold">Encuentra la propiedad de tus sueños con nosotros</h1>
         <p className="mt-4 text-lg">Con mas de 10 años de experiencia en el mercado..</p>
-        <a href="/propiedades" className="mt-6 inline-block bg-Background-color-nav text-Color-Text px-6 py-3 rounded-lg font-semibold">
+        <a href="/propiedades" className="mt-6 inline-block bg-Background-color-nav text-Color-Text px-6 py-3 rounded-lg font-semibold hover:bg-white hover:text-Background-color-nav duration-300">
           Ver Propiedades
         </a>
       </div>
