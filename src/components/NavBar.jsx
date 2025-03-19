93aa86486b02d6427d79874c139097edba9ae53e
DamianBermudez94 <bermudezdamian7@gamil.com>
2025-03-18 23:12:20 -0300

    se empezo a crear el diseño del menu hambuerguesa para movil

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,18 @@
 import LogoImagen from '../assets/logo.webp';
+import { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 export default function Navbar() {
+
+
+
+    const [menuOpen, setMenuOpen] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        setMenuOpen(false); // Cierra el menú al cambiar de ruta
+    }, [location.pathname]);
+
+
     return (
         <div className='bg-Background-color-nav flex justify-around p-4'>
             <div className='my-auto'>
@@ -7,12 +20,33 @@ export default function Navbar() {
                     <img className='w-[70%] h-[70%] object-cover' src={LogoImagen} alt="Mercedes Michelini | Negocios inmobiliarios" />
                 </a>
             </div>
-            <nav className=" text-white p-4 hidden md:block" >
-                <ul className="flex space-x-6">
-                    <li><a href="/home" className="hover:underline">Inicio</a></li>
-                    <li><a href="/propiedades" className="hover:underline">Propiedades</a></li>
-                    <li><a href="/acerca" className="hover:underline">Quienes somos</a></li>
-                    <li><a href="/contacto" className="hover:underline">Contacto</a></li>
+
+            <nav className="" >
+                <button
+                    id="menuButton"
+                    className="p-2"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >
+                    <div id="menuIcon" className="flex flex-col gap-1">
+                        <span
+                            className={`block w-6 h-0.5 bg-black transition-transform ${menuOpen ? "rotate-45 translate-y-1.5" : ""
+                                }`}
+                        ></span>
+                        <span
+                            className={`block w-6 h-0.5 bg-black transition-opacity ${menuOpen ? "opacity-0" : ""
+                                }`}
+                        ></span>
+                        <span
+                            className={`block w-6 h-0.5 bg-black transition-transform ${menuOpen ? "-rotate-45 -translate-y-1.5" : ""
+                                }`}
+                        ></span>
+                    </div>
+                </button>
+                <ul className={`absolute top-full left-0 bg-white w-full ${menuOpen ? "block" : "hidden"} md:block`}>
+                    <Link to="/home" data-section="section1" className="hover:underline">Inicio</Link>
+                    <Link to="/propiedades" data-section="section2" className="hover:underline">Propiedades</Link>
+                    <Link to="/acerca" data-section="section3" className="hover:underline">Quienes somos</Link>
+                    <Link to="/contacto" data-section="section4" className="hover:underline">Contacto</Link>
                 </ul>
             </nav>
         </div>
